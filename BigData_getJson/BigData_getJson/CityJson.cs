7db6ad5e3dd9fc2c2594cba5a9388e5cd56faf48
Vladislav_Pariyskiy <vilorchik@gmail.com>
2016-10-23 12:37:54 +0300

    updating

---
+++ BigData_getJson/BigData_getJson/CityJson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigData_getJson
+{
+
+    public class CityJson
+    {
+        public int _id { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public Coord coord { get; set; }
+    }
+
+    public class Coord
+    {
+        public float lon { get; set; }
+        public float lat { get; set; }
+    }
+
+}
--- BigData_getJson/BigData_getJson/Program.cs
+++ BigData_getJson/BigData_getJson/Program.cs
@@ -7,6 +7,7 @@ using RabbitMQ.Client;
 using Newtonsoft.Json;
 using System.Net;
 using System.IO;
+using System.Reflection;
 
 namespace BigData_getJson
 {
@@ -23,14 +24,14 @@ namespace BigData_getJson
         }
     
 
-    private static void SetupSerialisationMessageQueue(IModel model)
+        private static void SetupSerialisationMessageQueue(IModel model)
         {
             model.QueueDeclare(CommonService.SerialisationQueueName, false, false, false, null);
         }
 
         private static void RunSerialisationDemo(IModel model)
         {
-            var url = "http://api.openweathermap.org/data/2.5/weather?q=Volgograd,ru&units=metric&APPID=43bef92309c42a83f24c2a114bc3fcba";
+            var url = "http://api.openweathermap.org/data/2.5/weather?id=" + Convert.ToString(GetIdCity()) + "&units=metric&APPID=43bef92309c42a83f24c2a114bc3fcba";
             var request = (HttpWebRequest)WebRequest.Create(url);
             var response = (HttpWebResponse)request.GetResponse();
             var reader = new StreamReader(response.GetResponseStream());
@@ -43,6 +44,21 @@ namespace BigData_getJson
             byte[] customerBuffer = Encoding.UTF8.GetBytes(weather);
             model.BasicPublish("", CommonService.SerialisationQueueName, basicProperties, customerBuffer);
             
+        }
+        private static int GetIdCity()
+        {
+            StreamReader streamreader = new StreamReader("city.json");
+            Random rnd = new Random();
+            int r = rnd.Next(1, 209580);
+            for (int i = 1; i < r; i++)
+            {
+                streamreader.ReadLine();
+            }
+            string str = streamreader.ReadLine();
+            var city = JsonConvert.DeserializeObject<CityJson>(str);
+            int cityid = city._id;
+            streamreader.Dispose();
+            return cityid;
         }
     }
 }
